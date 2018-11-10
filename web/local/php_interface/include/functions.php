0cdc4b102cb1cf7cd4ed825dca6cd8698c4c2be6
Vitaly Nokhrin <vin@crtweb.ru>
2018-11-10 15:37:59 +0500

    add file init.php and file functions

---
+++ web/local/php_interface/include/functions.php
@@ -0,0 +1,30 @@
+<?php
+
+use Bitrix\Iblock\IblockTable;
+
+/**
+ * Возвращает ID инфоблока по его коду
+ *
+ * @param string $code - код ИБ
+ *
+ * @return int - ID найденного ИБ
+ * @throws Exception
+ */
+function getIblockIdByCode(string $code): int
+{
+    $iblock = IblockTable::getList([
+        'filter' => [
+            'CODE' => $code,
+        ],
+        'select' => [
+            'ID',
+            'CODE',
+        ],
+    ])->fetch();
+
+    if (!isset($iblock['ID'])) {
+        throw new Exception("Не найден инфоблок с кодом {$code}");
+    }
+
+    return (int) $iblock['ID'];
+}
