0d5258265623944074b4b422388ed26ebc3f09b8
Vitaly Nokhrin <vin@crtweb.ru>
2018-11-24 12:06:04 +0500

    add 404 page

--- web/404.php
+++ web/404.php
@@ -7,18 +7,19 @@ CHTTP::SetStatus("404 Not Found");
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 
 $APPLICATION->SetTitle("404 Not Found");
-
-$APPLICATION->IncludeComponent("bitrix:main.map", ".default", Array(
-	"LEVEL"	=>	"3",
-	"COL_NUM"	=>	"2",
-	"SHOW_DESCRIPTION"	=>	"Y",
-	"SET_TITLE"	=>	"Y",
-	"CACHE_TIME"	=>	"36000000"
-	)
-);
 ?>
-404
-
-
+<div class="error-area ptb-120">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="error-content text-center">
+                    <h1>404</h1>
+                    <p>Страница, которую вы ищете, не найдена!</p>
+                    <a href="index.html" class="btn">На главную</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 <?php
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
